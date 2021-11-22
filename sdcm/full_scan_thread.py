8aaa040a3ad360fb70987977227fa14b6cbe9599
Yaron Gilor <yarongilor@scylladb.com>
2021-11-22 21:25:50 +0200

    improvement(full-scan): Add support for reversed queries
    	Added an option to existing full-scan stress of issuing a reversed query
    	for a specified table random partition key.

--- sdcm/full_scan_thread.py
+++ sdcm/full_scan_thread.py
@@ -2,11 +2,12 @@ import logging
 import random
 import threading
 import time
+from typing import Optional
 
 from cassandra import ConsistencyLevel
 from cassandra.query import SimpleStatement  # pylint: disable=no-name-in-module
 
-from sdcm.cluster import BaseNode, BaseScyllaCluster, BaseCluster
+from sdcm.cluster import BaseNode, BaseScyllaCluster, BaseCluster, get_partition_keys
 from sdcm.sct_events import Severity
 from sdcm.sct_events.database import FullScanEvent
 
@@ -16,22 +17,29 @@ ERROR_SUBSTRINGS = ("timed out", "unpack requires", "timeout")
 
 # pylint: disable=too-many-instance-attributes
 class FullScanThread:
-    query_options = (
-        'select * from {}',
-        'select * from {} bypass cache'
-    )
-
     # pylint: disable=too-many-arguments
     def __init__(self, db_cluster: [BaseScyllaCluster, BaseCluster], ks_cf: str, duration: int, interval: int,
-                 termination_event: threading.Event, page_size: int = 100000):
+                 termination_event: threading.Event, page_size: int = 100000, allow_reversed_queries: bool = False):
         self.ks_cf = ks_cf
         self.db_cluster = db_cluster
         self.page_size = page_size
         self.duration = duration
         self.interval = interval
+        self.allow_reversed_queries = allow_reversed_queries
+        self.query_options = self.generate_query_options()
         self.termination_event = termination_event
         self.log = logging.getLogger(self.__class__.__name__)
         self._thread = threading.Thread(daemon=True, name=self.__class__.__name__, target=self.run)
+
+    def generate_query_options(self) -> dict:
+        bypass_cache = ' bypass cache'
+        basic_query = 'select * from {}'
+        reversed_query_suffix = ' where pk = {} order by ck desc'
+        reversed_query = basic_query + reversed_query_suffix
+        query_options = {'normal_query_options': [basic_query, basic_query + bypass_cache]}
+        if self.allow_reversed_queries:
+            query_options['reversed_query_options'] = [reversed_query, reversed_query + bypass_cache]
+        return query_options
 
     def get_ks_cs(self, db_node: BaseNode):
         ks_cf_list = self.db_cluster.get_non_system_ks_cf_list(db_node)
@@ -50,8 +58,17 @@ class FullScanThread:
             cmd += cql_timeout_param
         return cmd
 
-    def randomly_form_cql_statement(self, ks_cf: str) -> str:
-        cmd = random.choice(self.query_options).format(ks_cf)
+    def randomly_form_cql_statement(self, ks_cf: str, db_node: BaseNode) -> Optional[str]:
+        query_mode = random.choice(list(self.query_options.keys()))
+        if query_mode == 'reversed_query_options':
+            if pks := get_partition_keys(ks_cf=ks_cf, db_node=db_node):
+                partition_key = random.choice(pks)
+                cmd = random.choice(self.query_options['reversed_query_options']).format(ks_cf, partition_key)
+            else:
+                self.log.info('No partition keys found for table: %s! A reversed query cannot be executed!', ks_cf)
+                return None
+        else:
+            cmd = random.choice(self.query_options[query_mode]).format(ks_cf)
         return self.randomly_add_timeout(cmd)
 
     def create_session(self, db_node: BaseNode):
@@ -63,8 +80,9 @@ class FullScanThread:
         ks_cf = self.get_ks_cs(db_node)
         read_pages = random.choice([100, 1000, 0])
         with FullScanEvent(node=db_node.name, ks_cf=ks_cf, message="") as fs_event:
-            cmd = self.randomly_form_cql_statement(ks_cf)
-
+            cmd = self.randomly_form_cql_statement(ks_cf=ks_cf, db_node=db_node)
+            if not cmd:
+                return
             with self.create_session(db_node) as session:
 
                 if self.termination_event.is_set():
