b2ad0705744eccda5c4881cdd743d1799a17cc40
Yaron Gilor <yarongilor@scylladb.com>
2021-12-07 09:19:30 +0200

    Comment out Deepdif code

--- sdcm/scan_operation_thread.py
+++ sdcm/scan_operation_thread.py
@@ -5,7 +5,7 @@ import threading
 import time
 from abc import abstractmethod
 from typing import Optional
-# from deepdiff import DeepDiff
+# from deepdiff import DeepDiff  # TODO: uncomment when hydra docker is updated with DeepDif
 from cassandra import ConsistencyLevel
 from cassandra.query import SimpleStatement  # pylint: disable=no-name-in-module
 
@@ -266,14 +266,14 @@ class FullPartitionScanThread(ScanOperationThread):
         normal_query, reversed_query = queries
         ScanOperationThread.run_scan_operation(self, scan_operation_event=scan_operation_event, cmd=reversed_query)
         if self.full_partition_scan_params.get('validate_data'):
-            return  # TODO: remove when hydra docker is updated with DeepDif
-            reversed_query_result = self.query_result_data
-            ScanOperationThread.run_scan_operation(self, scan_operation_event=scan_operation_event, cmd=normal_query)
-            diff = DeepDiff(t1=reversed_query_result, t2=self.query_result_data, ignore_order=True,
-                            ignore_numeric_type_changes=True)
-            if diff:
-                InfoEvent(  # TODO: should it be an error event?
-                    message=f'Found the following differences between a reversed and normal queries: {diff}').publish()
+            pass  # TODO: remove and uncomment the below when hydra docker is updated with DeepDif
+            # reversed_query_result = self.query_result_data
+            # ScanOperationThread.run_scan_operation(self, scan_operation_event=scan_operation_event, cmd=normal_query)
+            # diff = DeepDiff(t1=reversed_query_result, t2=self.query_result_data, ignore_order=True,
+            #                 ignore_numeric_type_changes=True)
+            # if diff:
+            #     InfoEvent(
+            #         message=f'Found the following differences between a reversed and normal queries: {diff}').publish()
 
     def run(self):
         self.run_for_a_duration(scan_operation_event=FullPartitionScanReversedOrderEvent)
