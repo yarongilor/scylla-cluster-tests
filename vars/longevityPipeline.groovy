73f4f78e4195a10057088557f272ab9ff8305d1e
yarongilor <yarongilor@scylla.com>
2023-08-24 10:28:32 +0300

    longevityPipeline scylla-qa-internal compare_mv_base_table_sync

--- vars/longevityPipeline.groovy
+++ vars/longevityPipeline.groovy
@@ -231,7 +231,12 @@ def call(Map pipelineParams) {
                     dir('scylla-cluster-tests') {
                         timeout(time: 5, unit: 'MINUTES') {
                             checkout scm
-                            checkoutQaInternal(params)
+
+                            dir("scylla-qa-internal") {
+                                git(url: 'git@github.com:scylladb/scylla-qa-internal.git',
+                                    credentialsId:'b8a774da-0e46-4c91-9f74-09caebaea261',
+                                    branch: 'compare_mv_base_table_sync')
+                            }
                         }
                     }
                     dockerLogin(params)
