6081b8feb96c9c50e960b3d2309e4b6c1d5cb57c
yarongilor <yarongilor@scylla.com>
2024-12-05 12:03:52 +0200

    DBG2

--- tablets_split_merge_test.py
+++ tablets_split_merge_test.py
@@ -66,78 +66,34 @@ class TabletsSplitMergeTest(LongevityTest):
             InfoEvent(message="The Stress is not running. Rerunning it.").publish()
             self._run_all_stress_cmds(stress_queue, stress_params)
 
-    def test_tablets_split_merge(self):  # pylint: disable=too-many-locals  # noqa: PLR0914
-        """
-        (1) writing initial schema and get initial number of tablets.
-        (2) start write and read stress simultaneously.
-        (3) wait for tablets split.
-        (4) wait for stress completion.
-        (5) run deletions, nodetool flush and major compaction.
-        (6) wait for tablets merge, following a shrunk dataset size.
-        (7) redo more such cycles.
-        """
+    def test_tombstone_gc(self):  # pylint: disable=too-many-locals  # noqa: PLR0914
 
         # (1) initiate schema.
-        InfoEvent(message=f"Create a keyspace with tablets initial value").publish()
+        InfoEvent(message=f"Create a keyspace ").publish()
         self._pre_create_large_partitions_schema()
         stress_cmd = self.params.get('stress_cmd')
-        stress_read_cmd = self.params.get('stress_read_cmd')
-        deletion_percentage = 96  # How many of dataset partitions should be deleted.
+        deletion_percentage = 20  # How many of dataset partitions should be deleted.
 
         # Run prepare stress
         self.run_prepare_write_cmd()
 
         # Run Read background stress
-        stress_read_queue = []
-        read_params = {'keyspace_num': 1, 'stress_cmd': stress_read_cmd}
-        self._run_all_stress_cmds(stress_read_queue, read_params)
         self.node1 = self.db_cluster.data_nodes[0]
-        list_file_size_script_name = "list_files_and_sizes_to_file.py"
-        list_file_size_output_file = "/tmp/files_and_sizes.txt"
-        table_dst_path = os.path.join(tempfile.mkdtemp(prefix='tablets-split-merge'), 'files_and_sizes.txt')
 
-        for node in self.db_cluster.data_nodes:  # copy python script of calculating cf dir files size to db nodes.
-            node.remoter.send_files(f'./data_dir/{list_file_size_script_name}', '/tmp/')
+        if tombstone_gc_verification_params := self._get_tombstone_gc_verification_params():
+            self.run_tombstone_gc_verification_thread(**tombstone_gc_verification_params)
 
-        def _check_table_file_sizes():
-            for node in self.db_cluster.data_nodes:
-                InfoEvent(
-                    message=f"Checking file sizes of {KS_CF} directory on node {node.name}").publish()
-                node.remoter.run(f'python3 /tmp/{list_file_size_script_name}', verbose=True)
-                node.remoter.receive_files(src=list_file_size_output_file,
-                                           dst=table_dst_path)
-                try:
-                    with open(table_dst_path, 'r', encoding='utf-8') as file:
-                        self.log.debug(f"--- File sizes on node {node.name} ---")
-                        for line in file:
-                            self.log.debug(line.strip())
-                        self.log.debug(f"--- End of file sizes from node {node.name} ---")
-                except Exception as e:  # pylint: disable=broad-except  # noqa: BLE001
-                    self.log.error(
-                        f"Failed to read the output file {table_dst_path} from node {node.name}. Error: {e}"
-                    )
-
-        InfoEvent(message=f"Starting background thread to query and set max tablets number.").publish()
-        background_thread = threading.Thread(target=self._background_query_tablets_num, daemon=True)
-        background_thread.start()
         # Run cycles of writes and deletions
-        cycles_num = 7
+        cycles_num = 5
         for cycle in range(cycles_num):
-            with self.tablets_num_lock:
-                initial_tablets_num = self.max_tablets_num = self._get_tablets_number()
-            InfoEvent(
-                message=f"Cycle {cycle }, Initial tablets number before stress is: {initial_tablets_num}").publish()
             InfoEvent(message=f"Starting write load: {stress_cmd}").publish()
             stress_queue = []
             self.assemble_and_run_all_stress_cmd(stress_queue=stress_queue, stress_cmd=stress_cmd, keyspace_num=1)
-
-            self._start_stress_if_not_running(stress_read_queue, read_params)
 
             InfoEvent(message="Wait for write stress to finish (if running)").publish()
             for stress in stress_queue:
                 self.verify_stress_thread(cs_thread_pool=stress)
 
-            self._start_stress_if_not_running(stress_read_queue, read_params)
             InfoEvent(message=f"Start deletions to trigger a tablets merge.").publish()
             self.delete_partitions_in_batch(deletion_percentage)
 
@@ -146,36 +102,13 @@ class TabletsSplitMergeTest(LongevityTest):
                         for node in self.db_cluster.data_nodes]
             ParallelObject(objects=triggers, timeout=2400).call_objects()
 
-            InfoEvent(
-                message=f"Checking file sizes of {KS_CF} directory on nodes before major compaction").publish()
-            _check_table_file_sizes()
-
-            InfoEvent(message=f"Run a major compaction for {KEYSPACE_NAME} on nodes").publish()
-            triggers = [partial(node.run_nodetool, sub_cmd="compact", args=f"{KEYSPACE_NAME} {TABLE_NAME}", ) for
+            InfoEvent(message=f"Run a repair for {KEYSPACE_NAME} on nodes").publish()
+            triggers = [partial(node.run_nodetool, sub_cmd="repair", args=KEYSPACE_NAME, ) for
                         node in self.db_cluster.data_nodes]
             ParallelObject(objects=triggers, timeout=2400).call_objects()
 
-            InfoEvent(
-                message=f"Checking file sizes of {KS_CF} directory on nodes after major compaction").publish()
-            _check_table_file_sizes()
-
-            self._wait_for_tablet_split(tablets_num=initial_tablets_num)
             self.log.debug(f"Final max tablets number: {self.max_tablets_num}")
 
-            if self.max_tablets_num <= initial_tablets_num:
-                InfoEvent(
-                    message=f"The maximum number of tablets [{self.max_tablets_num}] is not bigger than the initial number [{initial_tablets_num}].", severity=Severity.ERROR).publish()
-            InfoEvent(message="Wait for tablets merge after deletion is done.").publish()
-            self._wait_for_tablets_merge()
-
-        self.log.debug("Stopping background thread.")
-        self.query_tablets_num_stop_event.set()
-        background_thread.join()
-
-        InfoEvent(message="Wait for read stress to finish.").publish()
-
-        for stress in stress_read_queue:
-            self.verify_stress_thread(cs_thread_pool=stress)
 
     def _is_stress_finished(self, stress_queue) -> bool:
         for index, stress in enumerate(stress_queue):
