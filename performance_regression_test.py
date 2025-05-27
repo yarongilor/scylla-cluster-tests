1bd6b788514deacf5e4b958f694d48c157976004
yarongilor <yarongilor@scylla.com>
2025-05-27 14:50:54 +0300

    test(debug): debug sct yaml
    debug sct yaml: instance_type_db: 'i3.large'

--- performance_regression_test.py
+++ performance_regression_test.py
@@ -330,7 +330,8 @@ class PerformanceRegressionTest(ClusterTester, loader_utils.LoaderUtilsMixin):
         stress_queue = self.run_stress_thread(stress_cmd=stress_cmd, stress_num=1, stats_aggregate_cmds=False)
         if nemesis:
             interval = self.params.get('nemesis_interval')
-            time.sleep(interval * 60)  # Sleeping one interval (in minutes) before starting the nemesis
+            time.sleep(80)
+            # time.sleep(interval * 60)  # Sleeping one interval (in minutes) before starting the nemesis
             self.db_cluster.add_nemesis(
                 nemesis=self.get_nemesis_class(), tester_obj=self, hdr_tags=stress_queue.hdr_tags)
             self.db_cluster.start_nemesis(interval=interval, cycles_count=1)
